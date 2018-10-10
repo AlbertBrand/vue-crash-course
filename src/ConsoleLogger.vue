41bdd9a10d1e146719cb5709cd7fdbb5863488fd
Albert Brand <abrand@xebia.com>
2018-10-10 11:44:06 +0200

    Improve console logger with autoscroll and colors

--- src/ConsoleLogger.vue
+++ src/ConsoleLogger.vue
@@ -1,14 +1,17 @@
 <template>
-  <textarea v-model="message"></textarea>
+  <section class="console" ref="log">
+    <div v-for="log in logs" :key="log.id" :class="log.type">{{log.line}}</div>
+  </section>
 </template>
 
 <script>
 export default {
   data: () => ({
-    message: '',
+    logs: [],
+    nextId: 1,
   }),
   beforeCreate: function() {
-    function wrap(vm, original) {
+    function wrap(vm, type, original) {
       return function() {
         const args = [];
         for (let i = 0; i < arguments.length; i++) {
@@ -19,12 +22,32 @@ export default {
             args.push(arg);
           }
         }
-        vm.message += args.join(' ') + '\n';
+        vm.logs.push({
+          id: vm.nextId++,
+          line: args.join(' '),
+          type,
+        });
         original(...arguments);
       };
     }
-    console.log = wrap(this, console.log);
-    console.error = wrap(this, console.error);
+    console.log = wrap(this, 'log', console.log);
+    console.error = wrap(this, 'error', console.error);
+  },
+  updated() {
+    const elem = this.$refs.log;
+    elem.scrollTop = elem.scrollHeight;
   },
 };
 </script>
+
+<style scoped>
+.console {
+  overflow: scroll;
+  border: 1px solid #eee;
+  padding: 5px;
+  font-family: monospace;
+}
+.error {
+  color: red;
+}
+</style>
