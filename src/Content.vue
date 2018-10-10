49124aadc72fecaba4201df54300bb775b231f92
Albert Brand <abrand@xebia.com>
2018-10-10 13:10:14 +0200

    Add prev/next button

--- src/Content.vue
+++ src/Content.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="content">
-    <header>
+    <header class="header">
       <h1>{{component.name}}</h1>
+      <nav class="quick">
+        <router-link v-if="prevPath" :to="prevPath">⇦</router-link>
+        <span v-else>⇦</span>
+        <router-link v-if="nextPath" :to="nextPath">⇨</router-link>
+        <span v-else>⇨</span>
+      </nav>
     </header>
     <vuep :template="component.code" :key="'preview:' + component.name" :scope="scope" class="preview" />
     <console-logger :key="'log:' + component.name" class="logger" />
@@ -11,6 +17,7 @@
 <script>
 import Vue from 'vue';
 import ConsoleLogger from './ConsoleLogger.vue';
+import routes from './routes';
 
 export default {
   props: {
@@ -27,10 +34,26 @@ export default {
   components: {
     ConsoleLogger,
   },
+  computed: {
+    nextPath() {
+      return this.currentIdx < routes.length - 2
+        ? routes[this.currentIdx + 1].path
+        : undefined;
+    },
+    prevPath() {
+      return this.currentIdx > 0 ? routes[this.currentIdx - 1].path : undefined;
+    },
+    currentIdx() {
+      return routes.findIndex(route => route.path === this.$route.path);
+    },
+  },
 };
 </script>
 
 <style scoped>
+.header {
+  display: flex;
+}
 .content {
   flex: 1;
   display: flex;
@@ -39,6 +62,17 @@ export default {
 }
 h1 {
   margin: 0 0 10px;
+  flex: 1;
+}
+.quick * {
+  margin-left: 10px;
+  font-size: 30px;
+}
+.quick span {
+  color: #ccc;
+}
+.quick a {
+  text-decoration: none;
 }
 .preview {
   flex: 1;
